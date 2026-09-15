cff56f9a9cb8f6bb92e83c658839d42ccd4a25b9
Mitchell Wilson <mitchellwilson@Mitchells-MacBook-Pro.local>
2026-09-15 13:42:59 -0500

    Initial commit, new Vite react project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <section id="center">
+        <h1>Algorithms</h1>
+      </section>
+    </>
+  )
+}
+
+export default App
