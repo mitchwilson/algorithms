702ae8f57c53a59f26723363d34d011badc9b189
Mitchell Wilson <mitchellwilson@Mitchells-MacBook-Pro.local>
2026-09-15 16:54:07 -0500

    feat: add second reverse string example

--- src/pages/StringReverse.jsx
+++ src/pages/StringReverse.jsx
@@ -21,29 +21,55 @@ function StringReverse() {
   useEffect(()=>{
     setReversedStringMethod1(str=>[...string].reverse().join(''))
     setReversedStringMethod2(str=>{
-      return str
+      let newString = ""
+      for(const character of string) {
+        newString = character + newString
+      }
+      return newString
     })
   }, [string])
 
   return (
     <section>
       <h1>String Reverse</h1>
-      <p className="intro">Various JavaScript algorithms to reverse a string value. Type anywhere. Your string and the reversed string will appear below. Reload the page to restart.</p>
+      <p className="intro">Various JavaScript algorithms to reverse a string value. Type anywhere. <code className="user-string">Your string</code> and the <code className="reversed-string">reversed string</code> will appear below. Reload the page to restart.</p>
       <h2>Algorithm 01</h2>
-      <p>Using this code <code>[...string].reverse().join('')</code></p>
+      <p>Reverse the string using array methods:</p>
+      <pre>
+        <code>[...string].reverse().join('')</code>
+      </pre>
       <ol>
-        <li>Convert the string to an array</li>
-        <li>Use the <code className="alpha">reverse</code> method to reverse the array</li>
-        <li>Then use the <code>join</code> method to convert the array back into a string</li>
+        <li>Use the spread operator <code>...</code> to convert the string to an array.</li>
+        <li>Use the <code className="alpha">reverse</code> method to reverse the array.</li>
+        <li>Then use the <code>join</code> method to convert the array back into a string.</li>
       </ol>
-        <p>
-          <label>Your string</label>: <code className="alpha">{ string }</code>
-        </p>
-        <p>
-          <label>Reversed str</label>: <code>{ reversedStringMethod1 }</code>
-        </p>
+      <p>
+        <label>Your string</label>: <code className="user-string">{ string }</code>
+      </p>
+      <p>
+        <label>Reversed str</label>: <code className="reversed-string">{ reversedStringMethod1 }</code>
+      </p>
+      <br></br>
+      <h2>Algorithm 02</h2>
+      <p>Reverse the string using an interable and string concatenation:</p>
+      <pre>
+        <code>{`let newString = ''
+for (const character of string) {
+  newString = character + newString
+}`}</code>
+      </pre>
+      <ol>
+        <li>Create a new empty <code>newString</code> variable.</li>
+        <li>Interate over each <code>character</code> in the <code>string</code>.</li>
+        <li>Prepend each <code>character</code> to the beginning of the <code>newString</code>.</li>
+      </ol>
+      <p>
+        <label>Your string</label>: <code className="user-string">{ string }</code>
+      </p>
+      <p>
+        <label>Reversed str</label>: <code className="reversed-string">{ reversedStringMethod2 }</code>
+      </p>
     </section>
-    
   )
 }
 
