6ca4e634fe9391a3e55ea5920b92dc2de336459e
Mitchell Wilson <mitchellwilson@Mitchells-MacBook-Pro.local>
2026-09-16 02:41:58 -0500

    feat: added actual code logic to examples on the Integer Reverse page

--- src/pages/IntegerReverse.jsx
+++ src/pages/IntegerReverse.jsx
@@ -2,20 +2,16 @@ import { useEffect, useState } from 'react'
 import '../App.css'
 
 function IntegerReverse() {
-  const [reversedIntegerMethod1, setReversedIntegerMethod1] = useState('')
-  const [reversedStringMethod2, setReversedIntegerMethod2] = useState('')
-  const [string, setString] = useState('')
+  const [integer, setInteger] = useState(0)
 
   function handleKeypress(event) {
-    const character = Number(event.key)
+    const key = event.key
     
-    if(Number.isNaN(character)) return
+    if(Number.isNaN(Number(key))) return
 
-    setString(prevInt=>prevInt + character)
-  }
-
-  function reverseInt(int) {
-
+    setInteger(prevInt=>{
+      return Number(prevInt.toString() + key)
+    })
   }
 
   useEffect(()=>{
@@ -26,14 +22,15 @@ function IntegerReverse() {
     }
   }, [])
 
-  useEffect(()=>{
-    setReversedIntegerMethod1(int=>{
+  const reversedMethod1 = Number(integer.toString().split('').reverse().join(''))
+  let reversedMethod2 = 0
+  let remainingInteger = integer
 
-    })
-    setReversedIntegerMethod2(int=>{
-      
-    })
-  }, [string])
+  while (remainingInteger > 0) {
+    const lastDigit = remainingInteger % 10
+    reversedMethod2 = reversedMethod2 * 10 + lastDigit
+    remainingInteger = Math.floor(remainingInteger / 10)
+  }
 
   return (
     <section>
@@ -43,37 +40,46 @@ function IntegerReverse() {
       <br></br>
       
       <h2>Algorithm 01</h2>
-      <p>Reverse the integer using ... :</p>
+      <p>Reverse the integer by first converting it to a string, then by using a common reverse string algorithm. Once reversed, convert back to a number</p>
       <pre>
-        <code>{`[code]`}</code>
+        <code>{`const reversedString = integer.toString().split('').reverse().join('')
+const reversedNumber = Number(reversedString)`}</code>
       </pre>
       <ol>
-        <li>Step 1 ...</li>
-        <li>Step 2 ...</li>
-        <li>Step 3.</li>
+        <li>Convert the number to a string using the <code>toString</code> method.</li>
+        <li>Then convert the string to an array using the <code>split</code> method.</li>
+        <li>Then reverse the array using the <code>reverse</code> method.</li>
+        <li>Then covert it from an array and back to a string using the <code>join</code> method.</li>
+        <li>And finally covert the string back into a number using the <code>Number</code> type cast.</li>
       </ol>
       <p>
-        <label>Your integer</label>: <code className="user-string">{ string }</code>
+        <label>Your integer</label>: <code className="user-string">{ integer }</code>
       </p>
       <p>
-        <label>Reversed int</label>: <code className="reversed-string">{ reversedStringMethod2 }</code>
+        <label>Reversed int</label>: <code className="reversed-string">{ reversedMethod1 }</code>
       </p>
       <br></br>
       <h2>Algorithm 02</h2>
-      <p>Reverse the integer using ... :</p>
+      <p>Reverse the integer using a while loop and the modulo operator to get the last character.</p>
       <pre>
-        <code>[code]</code>
+        <code>{`let reversedMethod2 = 0
+let remainingInteger = integer
+while (remainingInteger > 0) {
+  const lastDigit = remainingInteger % 10
+  reversedMethod2 = reversedMethod2 * 10 + lastDigit
+  remainingInteger = Math.floor(remainingInteger / 10)
+}`}</code>
       </pre>
       <ol>
-        <li>Step 1 ...</li>
-        <li>Step 2 ...</li>
-        <li>Step 3.</li>
+        <li>Use the remainder operator to get the last digit.</li>
+        <li>Append that digit to the reversed integer.</li>
+        <li>Use <code>Math.floor</code> to remove the last digit.</li>
       </ol>
       <p>
-        <label>Your integer</label>: <code className="user-string">{ string }</code>
+        <label>Your integer</label>: <code className="user-string">{ integer }</code>
       </p>
       <p>
-        <label>Reversed int</label>: <code className="reversed-string">{ reversedIntegerMethod1 }</code>
+        <label>Reversed int</label>: <code className="reversed-string">{ reversedMethod2 }</code>
       </p>
     </section>
   )
